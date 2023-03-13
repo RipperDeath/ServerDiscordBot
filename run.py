b1f9c7ed33d0041a1bdcb401d0d98d6b046f0e0d
RipperDeath <jacklinmelo@protonmail.com>
2023-03-12 21:09:04 -0400

    fixed uptime on bot

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 import json
 import asyncio
+import datetime, time
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands, tasks
@@ -14,46 +15,37 @@ with open('config.json') as f:
 bot = commands.Bot(command_prefix=prefix, intents=discord.Intents.all()) #fuck it, idk what i need
 bot.remove_command("help")
 
-startup_extensions = []
-#loops through all the extensions
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        startup_extensions.append("cogs."+filename[:-3]) #removes .py from filename
+async def load_extensions():
+    startup_extensions = []
+    #loops through all the extensions
+    for filename in os.listdir('./discordbot/cogs'):
+        if filename.endswith('.py'):
+            startup_extensions.append("discordbot.cogs."+filename[:-3]) #removes .py from filename
 
-# starting extensions
-for extension in startup_extensions:
-    try:
-        bot.load_extension(extension)
-        print(f"{extension} loaded")
-    except Exception as e:
-        exc = '{}: {}'.format(type(e).__name__, e)
-        print(f'Failed to load extension {extension}\n{exc}')
+    # starting extensions
+    for extension in startup_extensions:
+        try:
+            await bot.load_extension(extension)
+            print(f"{extension} loaded")
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print(f'Failed to load extension {extension}\n{exc}')
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is now online!")
-    uptime.start()
+    starttime = time.time()
+    try:
+        uptime.start(starttime)
+    except RuntimeError:
+        pass
 
 # uptime counter
-@tasks.loop(seconds=1)
-async def uptime(): #uptime
-    await bot.wait_until_ready()
-    timeCounter = 0
-    counter = 0
-    while not bot.is_closed():
-        counter += 1
-        timeCounter += 1
-        #convert seconds to days, hours, minutes, seconds
-        days = int(timeCounter / 86400)
-        hours = int(timeCounter / 3600) % 24
-        minutes = int(timeCounter / 60) % 60
-        seconds = timeCounter % 60
-
-        await asyncio.sleep(1)
-        if counter == 60:
-            print(f"Uptime: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds")
-            counter = 0
+@tasks.loop(seconds=60)
+async def uptime(starttime): #uptime
+    print(f"Bot uptime: {datetime.timedelta(seconds=int(round(time.time()-starttime)))}")
 
 env_dict = dict(os.environ) #supposally this is faster than a regular call to os.environ
 #bot.loop.create_task(uptime())
+asyncio.run(load_extensions())
 bot.run(env_dict['TESTBOT'])
